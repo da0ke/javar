a024854fd5e500fade66f7841054c7c60cc0909c
zhoujie <592052922@qq.com>
2017-03-11 08:48:30 +0800

    创建MyTool

---
+++ javar/src/main/java/cn/javar/demo/tool/MyTool.java
@@ -0,0 +1,118 @@
+package cn.javar.demo.tool;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
+import javax.servlet.http.HttpServletRequest;
+
+public class MyTool {
+	
+	/**
+     * 验证手机号码格式
+     */
+    public static boolean isMatchesMobile(String mobile){
+    	if(mobile!=null && mobile.length()==11) {
+    		String rule = "^(((13[0-9])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8})|(0\\d{2}-\\d{8})|(0\\d{3}-\\d{7})$";
+    		if(mobile.matches(rule)) {
+        		return true;
+        	}
+    	}
+    	return false;
+    }
+    
+    /**
+     * 验证邮箱格式
+     */
+    public static boolean isMatchesEmail(String email){
+    	String rule = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+    	if(email.matches(rule)) {
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
+    
+    /**
+     * 验证密码格式
+     */
+    public static boolean isMatchesPassword(String password) {
+        String rule = "^[a-zA-Z0-9_][a-zA-Z0-9_]{5,17}$";//由字母、数字、下划线组成，6-18位
+        if(password.matches(rule)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+	/**
+	 * 获取IP
+	 */
+	public static String getIP(HttpServletRequest request) {
+		//获取IP
+		String realIp = request.getHeader("x-real-ip");
+		String forwarded = request.getHeader("x-forwarded-for");
+		String remoteAddr = request.getRemoteAddr();
+		String ip;
+		if(realIp!=null && !realIp.equals("")) {
+			ip = realIp;
+		} else if(forwarded!=null && !forwarded.equals("")) {
+			ip = forwarded;
+		} else if(remoteAddr!=null && !remoteAddr.equals("")) {
+			ip = remoteAddr;
+		} else {
+			ip = "0.0.0.0";
+		}
+
+		return ip;
+	}
+	
+	/**
+	 * 根据出生年份计算年龄
+	 * 虚岁
+	 */
+	public static int getAgeByBirth(int year) {
+		Calendar calendar = Calendar.getInstance();
+		int thisYear = calendar.get(Calendar.YEAR);
+		return (thisYear + 1 - year);
+	}
+	
+	/**
+	  * 生成随机短信验证码
+	  * 6位随机数字
+	  */
+	public static String generateSMSCode() {
+		int code = (int)((Math.random()*9+1)*100000);
+		return String.valueOf(code);
+	}
+	
+	/**
+	 * 取得md5值
+	 */
+    public static String getMd5(String plainText) {  
+        try {  
+            MessageDigest md = MessageDigest.getInstance("MD5");  
+            md.update(plainText.getBytes());  
+            byte b[] = md.digest();  
+  
+            int i;  
+  
+            StringBuffer buf = new StringBuffer("");  
+            for (int offset = 0; offset < b.length; offset++) {  
+                i = b[offset];  
+                if (i < 0)  
+                    i += 256;  
+                if (i < 16)  
+                    buf.append("0");  
+                buf.append(Integer.toHexString(i));  
+            }  
+            //32位加密  
+            return buf.toString().toUpperCase();  
+        } catch (NoSuchAlgorithmException e) {  
+            e.printStackTrace();  
+            return null;  
+        }  
+  
+    }  
+	
+	
+}
